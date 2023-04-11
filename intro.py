aabc9609c94f98a499c5103e5a269bfd5cfe1ab0
Burul18 <rustamovaburul@gmail.com>
2023-04-11 20:47:23 +0600

    added git instructions

--- intro.py
+++ intro.py
@@ -20,3 +20,10 @@
 
 # 5. git push <origin> <master> - команда отправки всех ваших изменений на удаленный репо
 
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# 1*) git init
+#         2) git add .
+#         3) git commit -m ''
+# 4*) git remote add origin <url>
+#         5) git push origin main
